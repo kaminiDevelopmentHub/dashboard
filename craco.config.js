42fa28e8143c89ea8fd077df160e48b7f3427925
Kamini Parmar <kaminiparmar@Kaminis-MacBook-Air.local>
2026-05-13 09:58:29 +0530

    Updated mfa remotes entry

--- craco.config.js
+++ craco.config.js
@@ -1,22 +1,30 @@
 const { ModuleFederationPlugin } = require("webpack").container;
 
 module.exports = {
-  webpack: {
-    configure: (webpackConfig) => {
-      webpackConfig.plugins.push(
-        new ModuleFederationPlugin({
-          name: "dashboard",
-          filename: "remoteEntry.js",
-          exposes: {
-            "./DashboardApp": "./src/App",
-          },
-          shared: {
-            react: { singleton: true },
-            "react-dom": { singleton: true },
-          },
-        })
-      );
-      return webpackConfig;
+    webpack: {
+        configure: (webpackConfig) => {
+            webpackConfig.output.publicPath = "auto";
+
+            webpackConfig.plugins.push(
+                new ModuleFederationPlugin({
+                    name: "dashboard",
+                    filename: "remoteEntry.js",
+                    exposes: {
+                        "./DashboardApp": "./src/App",
+                    },
+                    shared: {
+                        // react: {
+                        //     singleton: true,
+                        //     requiredVersion: false,
+                        // },
+                        // "react-dom": {
+                        //     singleton: true,
+                        //     requiredVersion: false,
+                        // },
+                    },
+                })
+            );
+            return webpackConfig;
+        },
     },
-  },
 };
